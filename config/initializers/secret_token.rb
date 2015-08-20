f43e38dee09fed9ef81c3118d536824fc42bf08d
Gretchen Halasi-Kun <g.halasikun@gmail.com>
2015-08-20 20:14:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petpicker::Application.config.secret_key_base = '2922716413947f988fda7cc91f6e64bd174854d004e818097547c9777d9e3ad645f5c8964b07d544be0d62e89e1f37062dc1f89b29eecc09fc83b96e501f007f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Petpicker::Application.config.session_store :cookie_store, key: '_petpicker_session'
