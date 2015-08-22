c3cde65301c2f80ad34d9ef2009c96fdd1af69b8
Gretchen Halasi-Kun <g.halasikun@gmail.com>
2015-08-22 02:19:25 +0000

    form works

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,5 +6,15 @@ class PetsController < ApplicationController
 	def new
 		@pet = Pet.new
 	end
-	
+
+	def create
+		Pet.create(pet_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def pet_params
+		params.require(:pet).permit(:name, :reason)
+	end
 end
